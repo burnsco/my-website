bf2cd206424042366ed4773702cc52420ec3f791
Corey Burns <coreymburns@gmail.com>
2020-04-19 11:50:18 -0400

    added styled c. and removed boiler code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Corey Burns`,
+    description: `Web Developer`,
+    author: `coreymburns@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
